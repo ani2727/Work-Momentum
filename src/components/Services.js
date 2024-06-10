c75b0a541c041c61e6b96799aa57883af208b444
Laxmikanth-060 <laxmikanthanubothula@gmail.com>
2024-06-10 19:48:09 +0530

    lucky's

--- src/components/Services.js
+++ src/components/Services.js
@@ -17,7 +17,7 @@ const Services = ()=>{
             <div className='webData'>
                 <h1 className='cloudHeading'>Cloud & DevOps</h1>
                 <p className='text'>Save 30% on tuition, learn the full product design process and become dual certified in both UX and UI with credit-rated distinction. </p>
-                <button className='btn'><h2>Book a Demo</h2></button> <button className='btn'><h2>enroll now for $100/mon</h2></button>
+                <button className='btn'><h2>Book Demo</h2></button> <button className='btn'><h2>enroll now for $100/mon</h2></button>
              </div>
           </div>
 
