8c264463ddc4828e893986d06b6ca56fb59d87d2
Ajay Reddy <ajayreddy.karedla@gmail.com>
2024-06-10 21:09:39 +0530

    first commit from ajay

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+const Header = () => {
+  return (
+    <header>
+      <img
+        alt="gen-workmomentum-logo"
+        className="logo"
+        src="Gen-workmomentum-logo.png"
+      />
+      <nav className="nav-items-container">
+        <NavLink to="/" className="nav-item" activeclassname="active">
+          Home
+        </NavLink>
 
+        <NavLink to="/products" className="nav-item" activeclassname="active">
+          Products
+        </NavLink>
 
-const Header = ()=>{
-    return(
-        <div>Header</div>
-    )
-}
+        <NavLink to="/services" className="nav-item" activeclassname="active">
+          Services
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
