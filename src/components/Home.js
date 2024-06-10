12a676c3c55dab04f925f9faf3f88f8d6b435894
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-06-10 21:22:41 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import {Link} from "react-router-dom"
 const Home = ()=>{
     return(
+        <div>
         <Link to="/products" ><div>Products</div></Link>
         <Link to="/services" ><div>Services</div></Link>
+        </div>
     )
 }
 
