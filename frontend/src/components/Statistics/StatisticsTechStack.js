6490470ece683605fe40d427679ad0d40eecdbfb
Katroth Anil <katrothanil123@gmail.com>
2024-07-05 11:47:26 +0530

    updated products tech stack images

--- frontend/src/components/Statistics/StatisticsTechStack.js
+++ frontend/src/components/Statistics/StatisticsTechStack.js
@@ -6,14 +6,14 @@ const StatisticsTechStack = ()=>{
             <div className="stat-card-tech">
                 <div className="stat-header tech">
                     <h1>CI/CD Tools</h1>
-                    <img src='https://images.prismic.io/hatica/271b0e2b-7a81-4421-b1fa-9d84715bc3fe_Top+10+CI+Pipeline+Tools.png?auto=compress,format&rect=0,0,1801,1151&w=1200&h=767' alt='fav' />
+                    <img src='https://i.postimg.cc/D0pg4820/image.png' alt='fav' />
                 </div>
                 <p>Jenkins, GitLab</p>
             </div>
             <div className="stat-card-tech">
                 <div className="stat-header tech">
                     <h1>Cloud Providers</h1>
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQePoLN_sLIrgyV5eHlZkfIcg-9eO-NmCnAp56uC9IAL3dxnHlxalxyuahKwzwpFlNRco&usqp=CAU' alt='fav' />
+                    <img src='https://cdn-icons-png.flaticon.com/512/4215/4215831.png' alt='fav' />
                 </div>
                 <p>AWS, Azure, GCP</p>
             </div>
@@ -27,14 +27,14 @@ const StatisticsTechStack = ()=>{
             <div className="stat-card-tech">
                 <div className="stat-header tech">
                     <h1>Configuration Management</h1>
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1Y2d20KTGkAO3euBPxUmsuINZm2fVxZ-4Q4EvsBw7OKhG_9C1yMsRDVcobDYBTH0OS8c&usqp=CAU' alt='fav' />
+                    <img src='https://cdn.iconscout.com/icon/premium/png-256-thumb/engine-crash-7692031-6281559.png?f=webp' alt='fav' />
                 </div>
                 <p>Ansible</p>
             </div>
             <div className="stat-card-tech">
                 <div className="stat-header tech">
                     <h1>Continuous Deployment</h1>
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPErc5xtnCxLch-6xpDxz4k6bXr_zl3h7isFrtrj4CSuL0fL_lTS-Jax-2O5ruDYgvvYk&usqp=CAU' alt='fav' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUph41h6yxauq_n3mN3iUali-Kot5aelWU85jpfaoqU4r0xwyrlqijMk_0tCS085l41k4&usqp=CAU' alt='fav' />
                 </div>
                 <p>ArgoCD</p>
             </div>
@@ -55,7 +55,7 @@ const StatisticsTechStack = ()=>{
             <div className="stat-card-tech">
                 <div className="stat-header tech">
                     <h1>Security</h1>
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSspBo63yCKEe2aDPSLRATiiT1bN_oyLmHkEuPuxO78daikLXe0kTj5NQZTRijMyI_wTM&usqp=CAU' alt='fav' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaNGHL0pEuqxTfZVistka3woeC-RcAITgIjoYg5qnLSUMyyyHTtVQKpeORb8lKjSyZXXg&usqp=CAU' alt='fav' />
                 </div>
                 <p>Integrated DevSecOps tools</p>
             </div>
