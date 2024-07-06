7f2780f6c1f2319c5e0b88b7e340b1375e510fa2
SwapnithKumar <swapnithmadire123@gmail.com>
2024-07-06 12:05:02 +0530

    Scroll to top added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
